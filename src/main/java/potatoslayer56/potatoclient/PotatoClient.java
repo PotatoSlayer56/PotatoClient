65f8f98bb18f0046e226eb6def3eafa25986ceab
PotatoSlayer56 <joshcongreve56@gmail.com>
2026-01-06 01:31:21 +1100

    Initial Commit
    Base fabric mod from fabric template generator

---
+++ src/main/java/potatoslayer56/potatoclient/PotatoClient.java
@@ -0,0 +1,12 @@
+package potatoslayer56.potatoclient;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PotatoClient implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
